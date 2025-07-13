d19b01a00ec0cccd0920493915ecf386bc0fcf38
Isabelly bohn <isabelly.bohn@escola.pr.gov.br>
2025-07-12 22:17:05 -0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
+
 function contatarWhatsApp(produto) {
-  const numero = "5599999999999"; // Substitua pelo seu número com DDD e país (ex: 55 + DDD + número)
+  const numero = "5541995469616"; // substitua com seu número real
   const mensagem = encodeURIComponent(`Olá! Tenho interesse no ${produto}.`);
   const url = `https://wa.me/${numero}?text=${mensagem}`;
+  window.open(url, "_blank");
+}
+
+function abrirInstagram() {
+  const url = "https://instagram.com/sua_loja"; // substitua com seu @
   window.open(url, "_blank");
 }
